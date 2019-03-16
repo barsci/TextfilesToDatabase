55a730bc3d4b52c947aa0a1da94680543bd24865
barsci <barsci@o2.pl>
2019-03-16 15:12:56 +0100

    XMLParser parse method

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,37 +1,38 @@
 package main;
 
 import model.Customer;
-import org.xml.sax.SAXException;
 import parsers.csvparser.CSVParser;
 import parsers.xmlparser.XMLParser;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-        try {
-            SAXParser saxParser = saxParserFactory.newSAXParser();
-            XMLParser handler = new XMLParser();
-            saxParser.parse(new File("C:/Users/Marcin/Downloads/dane-osoby.xml"), handler);
-            List<Customer> customers = handler.getCustomerList();
-            for(Customer c: customers) {
+
+        Scanner scanner = new Scanner(System.in);
+        String pathname = scanner.nextLine();
+
+        File file = new File(pathname);
+
+        if(pathname.endsWith(".xml")) {
+            XMLParser xmlParser = new XMLParser();
+            xmlParser.parseXML(file);
+            List<Customer> xmlCustomers = xmlParser.getCustomerList();
+            for (Customer cust : xmlCustomers) {
+                System.out.println(cust);
+            }
+        } else if (pathname.endsWith(".txt")){
+            CSVParser csvParser = new CSVParser();
+            csvParser.parse(file);
+            List<Customer> customers = csvParser.getCustomerList();
+            for (Customer c : customers) {
                 System.out.println(c);
             }
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println();
-        CSVParser csvParser = new CSVParser();
-        csvParser.parse(new File("C:/Users/Marcin/Downloads/dane-osoby.txt"));
-        List<Customer> customers = csvParser.getCustomerList();
-        for(Customer c: customers) {
-            System.out.println(c);
+            System.out.println("csv");
+        } else {
+            System.out.println("PODAJ PLIK Z ODPOWIEDNIM ROZSZERZENIEM!");
         }
     }
 }
